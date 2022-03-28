6e50cd87057893380086499ecb2c79411631f040
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-28 16:58:57 +0200

    add li

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,16 @@
+//salvo il valore di lista in listHtml
+const listHtml = document.getElementById('lista')
+
 //Scrivo un programma che stampi in console i numeri da 1 a 100.
 for (i = 1; i <= 100; i++) {
+
+    //creo i <li> di ul
+    let listItem = `<li>${i}</li>`
 
     //se un numero è divisibile per 3 e da 0 è multiplo di 3. (Uso l'operatore testo %, che mi permette di dividere due numeri, controllando se il resto è uguale a 0). Quindi Se i è divisibile per 3 e per 5, scrivo: FizzBuzz AlTRIMENTI SE è divisibile solo per 3, scrivo: frizz ALTRIMENTI SE è divisibile solo per 5, scrivo: buzz ALTRIMENTI scrivo: il numero.
     if ((i % 3 == 0) && (i % 5 == 0)) {
         console.log('FizzBuzz');
+        let listItem = 'ciao'
     } else if (i % 3 == 0) {
         console.log('frizz');
     } else if (i % 5 == 0) {
@@ -12,4 +19,6 @@ for (i = 1; i <= 100; i++) {
         console.log(i);
     }
 
+    //scrivo su html
+    listHtml.insertAdjacentHTML('beforeend', listItem)
 }
