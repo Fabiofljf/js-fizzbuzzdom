a33c23eb19959d89ad6413c0da86772d3d278c6d
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-28 15:57:57 +0200

    initial html, css and js

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+//Scrivo un programma che stampi in console i numeri da 1 a 100.
+for (i = 1; i <= 100; i++) {
+    console.log(i);
+
+    // //Per i multipli di 3 stampi “Fizz” al posto del numero
+    // // se un numero è divisibile per 3 e da 0 e multiplo
+    if (i / 3 == 0); {
+        console.log('frizz');
+    }
+}
