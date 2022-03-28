318dfa46ae8f70510c29cdf495eeb8c18d0c47ab
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-28 16:08:45 +0200

    frizz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 //Scrivo un programma che stampi in console i numeri da 1 a 100.
 for (i = 1; i <= 100; i++) {
-    console.log(i);
 
-    // //Per i multipli di 3 stampi “Fizz” al posto del numero
-    // // se un numero è divisibile per 3 e da 0 e multiplo
-    if (i / 3 == 0); {
+
+    //Per i multipli di 3 stampi “Fizz” al posto del numero
+    //se un numero è divisibile per 3 e da 0 e multiplo di 3 (operatore testo %, che mi permette di dividere due numeri, controllando se il resto è uguale a 0)
+    if (i % 3 == 0); {
         console.log('frizz');
     }
+    console.log(i);
 }
