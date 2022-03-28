f47075850fd186a07b4713b4aa0aba4ac717f54e
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-28 18:12:53 +0200

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,16 +16,16 @@ for (i = 1; i <= 100; i++) {
     //se un numero è divisibile per 3 e da 0 è multiplo di 3. (Uso l'operatore testo %, che mi permette di dividere due numeri, controllando se il resto è uguale a 0). Quindi Se i è divisibile per 3 e per 5, scrivo: FizzBuzz AlTRIMENTI SE è divisibile solo per 3, scrivo: frizz ALTRIMENTI SE è divisibile solo per 5, scrivo: buzz ALTRIMENTI scrivo: il numero.
     if ((i % 3 == 0) && (i % 5 == 0)) {
         console.log('FrizzBuzz');
-        listItem = `<li class="square fb">${frizzBuzzElement}</li>`
+        listItem = `<li class="square col fb">${frizzBuzzElement}</li>`
     } else if (i % 3 == 0) {
         console.log('frizz');
-        listItem = `<li class="square f">${frizzElement}</li>`
+        listItem = `<li class="square col f">${frizzElement}</li>`
     } else if (i % 5 == 0) {
         console.log('buzz');
-        listItem = `<li class="square b">${buzzElement}</li>`
+        listItem = `<li class="square col b">${buzzElement}</li>`
     } else {
         console.log(i);
-        listItem = `<li class="square numeri">${iElement}</li>`
+        listItem = `<li class="square col numeri">${iElement}</li>`
     }
     console.log(listItem)
 
